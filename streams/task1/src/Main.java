4d3a4ddef776166346c049d8425d555caaebd11f
Azamat Kulbarakov <mr.kulbarakov@gmail.com>
2021-12-02 12:14:08 +0500

    Streams task1 added

---
+++ streams/task1/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> positiveInts = new ArrayList<>();
+        for (Integer x : intList) {
+            if ((x > 0) && (x % 2 == 0)) positiveInts.add(x);
+        }
+        positiveInts.sort(Integer::compareTo);
+        positiveInts.forEach(System.out::println);
+    }
+}
