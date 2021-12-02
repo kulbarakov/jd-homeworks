1b9c25c09353e6a6849f31b9034dbe50c004577e
Azamat Kulbarakov <mr.kulbarakov@gmail.com>
2021-12-02 14:46:24 +0500

    files\task1 added

---
+++ files/task1/src/Main.java
@@ -0,0 +1,64 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder logSB = new StringBuilder();
+        File file = new File("Games\\src");
+        if (file.mkdir())
+            logSB.append(LocalDateTime.now()).append(" - Папка ").append(file.getName()).append(" создана\n");
+        file = new File("Games\\res");
+        if (file.mkdir())
+            logSB.append(LocalDateTime.now()).append(" - Папка ").append(file.getName()).append(" создана\n");
+        file = new File("Games\\savedgames");
+        if (file.mkdir())
+            logSB.append(LocalDateTime.now()).append(" - Папка ").append(file.getName()).append(" создана\n");
+        file = new File("Games\\temp");
+        if (file.mkdir())
+            logSB.append(LocalDateTime.now()).append(" - Папка ").append(file.getName()).append(" создана\n");
+        file = new File("Games\\src\\main");
+        if (file.mkdir())
+            logSB.append(LocalDateTime.now()).append(" - Папка ").append(file.getName()).append(" создана\n");
+        file = new File("Games\\src\\test");
+        if (file.mkdir())
+            logSB.append(LocalDateTime.now()).append(" - Папка ").append(file.getName()).append(" создана\n");
+        file = new File("Games\\src\\main\\Main.java");
+        try {
+            if (file.createNewFile())
+                logSB.append(LocalDateTime.now()).append(" - Файл ").append(file.getName()).append(" создан\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        file = new File("Games\\src\\main\\Utils.java");
+        try {
+            if (file.createNewFile())
+                logSB.append(LocalDateTime.now()).append(" - Файл ").append(file.getName()).append(" создан\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        file = new File("Games\\res\\drawables");
+        if (file.mkdir())
+            logSB.append(LocalDateTime.now()).append(" - Папка ").append(file.getName()).append(" создана\n");
+        file = new File("Games\\res\\vectors");
+        if (file.mkdir())
+            logSB.append(LocalDateTime.now()).append(" - Папка ").append(file.getName()).append(" создана\n");
+        file = new File("Games\\res\\icons");
+        if (file.mkdir())
+            logSB.append(LocalDateTime.now()).append(" - Папка ").append(file.getName()).append(" создана\n");
+        file = new File("Games\\temp\\temp.txt");
+        try {
+            if (file.createNewFile())
+                logSB.append(LocalDateTime.now()).append(" - Файл ").append(file.getName()).append(" создан\n");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (FileWriter fw = new FileWriter(file)) {
+            fw.write(logSB.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
