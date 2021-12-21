61f670306305f86a08133ecc4774e0d4ceafc283
Azamat Kulbarakov <mr.kulbarakov@gmail.com>
2021-12-21 17:38:37 +0500

    CSV_XML_JSON task1

---
+++ special_files/task1/src/main/java/Main.java
@@ -0,0 +1,57 @@
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+        List<Employee> list = parseCSV(columnMapping, fileName);
+        String jsonObject = listToJson(list);
+        System.out.println(jsonObject);
+    }
+
+    private static <T> String listToJson(List<T> list) {
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        Type listType = new TypeToken<List<T>>() {
+        }.getType();
+        String jsonString = gson.toJson(list, listType);
+        try (FileWriter file = new FileWriter("data.json")) {
+            file.write(jsonString);
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return jsonString;
+    }
+
+    private static List<Employee> parseCSV(String[] columnMapping, String fileName) {
+        List<Employee> result = new ArrayList<>();
+        try (CSVReader reader = new CSVReader(new FileReader(fileName))) {
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping(columnMapping);
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(reader)
+                    .withMappingStrategy(strategy)
+                    .build();
+            result = csv.parse();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return result;
+    }
+}
