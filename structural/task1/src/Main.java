af63e112755d986ced80ee14dc361189406fece0
unknown <@s1001-k331-15.russian.space>
2022-02-10 14:29:18 +0500

    structural tasks added

---
+++ structural/task1/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.mult(5,5));
+        System.out.println(calc.sum(5,20));
+        System.out.println(calc.pow(5,2));
+    }
+}
